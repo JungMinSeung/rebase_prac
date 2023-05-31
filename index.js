baf2aea871b0b8261a6fcb6f258e45e831694077
mai_2 <seuinggg@gmail.com>
2023-05-31 09:30:07 +0900

    child-first-commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('parent-second-commit');
 console.log('parent-first-commit');
 console.log('parent-second-commit');
+console.log('child - first commit');
+console.log('child - first commit');
